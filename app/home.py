0709cf6234425592531cbc4dd37b67a2ddb47007
thebadger2017 <peterjtomlinson@outlook.com>
2017-11-15 18:03:26 +0000

    2017-11-15 - Added template folder and file 2017-11-15 - Added static folder 2017-11-15 - Updated home.py

--- app/home.py
+++ app/home.py
@@ -2,10 +2,16 @@
 # A very simple Flask Hello World app for you to get started with...
 
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello from Flask!'
+
+@app.route('/hello')
+def hello():
+	user = { 'nickname': 'Peter' }	
+	return render_template('index.html', 
+				title='HELLO!',
+				user=user)
 
